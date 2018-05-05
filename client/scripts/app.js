9e78a63901eb674c5e002aeb4a69f03125e8f111
dundaswest <hello@eunbyulcho.me>
2018-05-05 15:58:39 -0700

    Refactor for bolding friend messages and adding rooms to dropdown.

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,49 +1,59 @@
 // YOUR CODE HERE:
+var app = {};
 $(document).ready(function () {
-  var app = {};
 
   app.init = function () {
-    this.handleUsernameClick();
+    this.fetch();
     this.handleSubmit();
+    this.handleUsernameClick();
+    $('.refresh').on('click', function () {
+      app.fetch();
+    });
     
-    var url = 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages';
-    this.fetch(url);
-
+    this._friends = {};
+    this._rooms = {};
   };
 
-  app.handleUsernameClick = function () {
-    $('.username').on('click', function(){
+  app.handleUsernameClick = function (event) {
+    $('#chats').on('click', '.username', function(event) {
+      var username = window.location.search.slice(10);
+      var friend = $(this).text();
       
+      if (!app._friends[username]) {
+        app._friends[username] = {[friend]: true};
+      } else if (!app._friends[username][friend]) {
+        app._friends[username][friend] = true;
+      } 
+      app.fetch();
+      
+      console.log(app._friends[username][friend]);
     });
   };
 
   app.handleSubmit = function () {
-    $('#send').submit(function(event){
-    
-      var roomname = $('#roomSelect').find(":selected").text();
-      var text = $("input[type = 'text']").val();
-      // text = `${text}`;
+    $('#send').submit(function(event) {
+      var roomname = $('#roomSelect').find(':selected').text();
+      var text = $('input[type = "text"]').val();
+      var username = window.location.search.slice(10);
       app.send({
-        username: '8TH FLOOR',
+        username: username,
         text: text,
         roomname: roomname,
+      }, () => {
+        app.fetch();
       });
-      var url = 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages';
-      app.fetch(url);
-      event.preventDefault();
+      event.preventDefault();   
     });
   };
-//action="javascript:alert();"
-  app.send = function (message) {
+
+  app.send = function (message, callback) {
     $.ajax({
       // This is the url you should use to communicate with the parse API server.
       url: 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages',
       type: 'POST',
       data: JSON.stringify(message),
       contentType: 'application/json',
-      success: function (data) {
-        console.log('chatterbox: Message sent');
-      },
+      success: callback,
       error: function (data) {
         // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
         console.error('chatterbox: Failed to send message', data);
@@ -51,14 +61,22 @@ $(document).ready(function () {
     });
   };
 
-  app.fetch = function (url) {
+  app.fetch = function () {
     $.ajax({
       // This is the url you should use to communicate with the parse API server.
-      url: 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages',
+      url: this.server,
       type: 'GET',
       data: 'order=-createdAt',
       success: data => {
-        data.results.forEach(message => app.renderMessage(message));
+        console.log(data);
+        app.clearMessages();
+        data.results.forEach(message => {
+          app.renderMessage(message);
+        });
+        $('#roomSelect').empty();
+        for (var room in app._rooms) {
+          app.renderRoom(room);
+        }
       },
       error: function (data) {
         // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
@@ -72,12 +90,28 @@ $(document).ready(function () {
   };
 
   app.renderMessage = function (message) {
+    
+    var loggedInUser = window.location.search.slice(10);
+    
+    
     var usernameDiv = $('<div>');
     usernameDiv.addClass('username');
-    usernameDiv.text(`${message.username}:`);
+    usernameDiv.text(`${message.username}`);
     
-    var text = $('<span>');
+    var text = $('<div>');
+    text.addClass('messageText');
     text.text(`${message.text}`);
+    if (app._friends[loggedInUser] && app._friends[loggedInUser][message.username]) {
+      text.addClass('bold');
+      console.log('changed', app._friends[loggedInUser]);
+    }
+    
+    var roomname = document.createElement('div');
+    $(roomname).addClass('roomname');
+    $(roomname).text(`room: ${message.roomname ? message.roomname : ''}`);
+    if (!app._rooms[message.roomname]) {
+      app._rooms[message.roomname] = true;
+    }
     
     var date = $('<div>');
     var time = moment(message.createdAt).format('MMMM Do YYYY, h:mm:ss a');
@@ -86,10 +120,9 @@ $(document).ready(function () {
     var messageContainer = $('<div>');
     messageContainer.addClass('chat');
     messageContainer.append(usernameDiv);
+    messageContainer.append(roomname);
     messageContainer.append(text);
     messageContainer.append(date);
-    
-    
     
     $('#chats').append(messageContainer);
   };
@@ -99,6 +132,8 @@ $(document).ready(function () {
     lobbyDiv.text(lobbyName);
     $('#roomSelect').append(lobbyDiv);
   };
-
+  
+  app.server = 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages';
+  
   app.init();
 });
